d86572777304ca64c9a5621bb3504cc95b49f8b2
Col939 <87954615+Col939@users.noreply.github.com>
2021-09-09 11:05:09 -0400

    Update code.py

--- code.py
+++ code.py
@@ -4,6 +4,20 @@ x = 0
 
 def startGame():
   x = random.randint(0, 100)
-  
-  
-print(x)
+  print(x)
+startGame()  
+
+def checkGuess():
+    
+    guess = input("What is the Number?")
+    intGuess = int(guess)
+    
+    if intGuess == x:
+        
+        print("You Win!")
+    else:
+        print(intGuess == x)
+        print("Try Again :(")
+        checkGuess()
+        
+checkGuess()
